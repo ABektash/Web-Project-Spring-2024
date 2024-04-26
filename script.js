bea2a1c97e72717f73093a149c91ac4012b685a3
Youssef-Abdelshahid <youssefmoshahid@gmail.com>
2024-04-26 14:54:10 +0300

    Added the first section in the home page

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//Youssef Abdelshahid
+var timeout;
+
+function hideIntro() {
+    var introDiv = document.getElementById("intro");
+    introDiv.style.opacity = "0"; 
+    setTimeout(function() {
+        introDiv.style.display = "none";
+    }, 500); 
+}
+
+
+document.addEventListener("mousemove", function() {
+    var introDiv = document.getElementById("intro");
+    introDiv.style.display = "block";
+    introDiv.style.opacity = "1"; 
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(hideIntro, 5000); 
+});
