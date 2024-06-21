3b0cd4c5db905bf16fdf0e845fb896a3691229d4
A_Bektash <ammaralaa2005@gmail.com>
2024-06-22 02:48:41 +0300

    Fixed Design issues in Tickets page and fully configured it to work with the database.

--- controllers/TicketsController.js
+++ controllers/TicketsController.js
@@ -1,3 +1,11 @@
-exports.getTicketsPage = (req, res) => {
-    res.render('pages/Tickets', {user: req.session.user});
+const Ticket = require('../models/Ticket');
+
+exports.getTicketsPage = async (req, res) => {
+    try {
+        const tickets = await Ticket.find(); // Fetch all tickets from the database
+        res.render('pages/Tickets', { user: req.session.user, tickets }); // Pass tickets to the EJS template
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 };
