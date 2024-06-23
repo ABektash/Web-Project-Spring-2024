897c265d69863ce4bc91f93752eeeb31a2e387a7
Youssef-Abdelshahid <youssefmoshahid@gmail.com>
2024-06-23 15:14:38 +0300

    Fixed issues with BuyPage validations and made it work with tickets and add it to purchased tickets in the user database

--- routes/BuyPage.js
+++ routes/BuyPage.js
@@ -4,5 +4,7 @@ const BuyPageController = require('../controllers/BuyPageController');
 
 router.get('/', BuyPageController.getBuyPage);
 router.post('/', BuyPageController.postBuyPage);
+router.get('/:id', BuyPageController.getBuyPage);
+router.post('/:id', BuyPageController.postBuyTicketPage);
 
 module.exports = router;
